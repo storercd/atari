510405c5e6f490b1124d98a78f7fd013f0f8235f
Zane McFate <zanemcfate@gmail.com>
2019-11-17 11:12:48 -0800

    Fixed help text for game modes

--- atari.py
+++ atari.py
@@ -62,7 +62,7 @@ class Atari:
         parser = argparse.ArgumentParser()
         available_games = list((''.join(x.capitalize() or '_' for x in word.split('_')) for word in atari_py.list_games()))
         parser.add_argument("-g", "--game", help="Choose from available games: " + str(available_games) + ". Default is 'breakout'.", default="Breakout")
-        parser.add_argument("-m", "--mode", help="Choose from available modes: ddqn_train, ddqn_test, ge_train, ge_test. Default is 'ddqn_training'.", default="ddqn_training")
+        parser.add_argument("-m", "--mode", help="Choose from available modes: ddqn_training, ddqn_testing, ge_training, ge_testing. Default is 'ddqn_training'.", default="ddqn_training")
         parser.add_argument("-r", "--render", help="Choose if the game should be rendered. Default is 'False'.", default=False, type=bool)
         parser.add_argument("-tsl", "--total_step_limit", help="Choose how many total steps (frames visible by agent) should be performed. Default is '5000000'.", default=5000000, type=int)
         parser.add_argument("-trl", "--total_run_limit", help="Choose after how many runs we should stop. Default is None (no limit).", default=None, type=int)
